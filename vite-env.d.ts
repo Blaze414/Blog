7e9afcea3898eaa347149ffcb7efde8c9312b55b
YusufAlZadid <139882679+YusufAlZadid@users.noreply.github.com>
2026-08-19 18:49:59 +1000

    Made changes

--- vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "virtual:local-preview-articles" {
-  import type { BlogPost } from "./app/content/types";
-
-  export const localPreviewArticles: readonly BlogPost[];
-}
